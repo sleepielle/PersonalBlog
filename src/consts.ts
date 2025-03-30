84b54e1164e2e5416d6c35434982d02bdfb76118
sleepielle <pggisselle@gmail.com>
2025-03-30 01:28:21 -0600

    feat: Removed unused files and updated Astro configuration and layout files

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "TECHPEOPLE BLOG";
+export const SITE_DESCRIPTION =
+  "Articles, stories and tutorials from Tech People";
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
